e693c0407f122d6f9bafc309bb5cc9946ce96fb1
Daniel Muehlbachler <daniel@muehlbachler.org>
2012-01-06 13:11:58 +0100

    Initial commit

---
+++ README.txt
@@ -0,0 +1,67 @@
+-------------------
+How does this work?
+-------------------
+
+Because of some phishing related issues with Apple I decided to not host the script anymore.
+As a matter of fact, you have to run it on your own and I will provide you a short howto on how you can do this easily. 
+
+If this script helps you, please consider at least a small donation (https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=JVTUEYXWG76MA). Thank you for your support!
+
+
+----------------------------------------
+How can you run this script on your own?
+----------------------------------------
+
+At first, you need a working webserver with PHP and cURL.
+The easiest way to get this is the XAMPP project (http://www.apachefriends.org/de/xampp.html). There you can download an Apache/MySQL and PHP bundle for your Windows, Mac or Linux computer.
+With that bundle it is really easy to get the script running:
+
+    Download the bundle (I recommend the packed version and not the executable/self-install one) and unpack it.
+    Now it depends on the version you are using:
+        Windows: goto the php folder and open the file php.ini.
+        Linux: goto the etc/php folder and open the file php.ini.
+        Mac: goto the xamppfiles/etc/php folder and open the file php.ini.
+    In this file search for "curl" and uncomment this line where it appears. (e.g. extension=php_curl.dll in Windows)
+    Download my script (download link: see later), unpack it and copy the file icloud.php into the htdocs folder of your XAMPP installation.
+    Now start Apache depending on the version you are using:
+        Windows: double click on apache_start.bat or open xampp-control.exe and start Apache by clicking on the appropriate button
+        Linux: please read through the XAMPP documentation - but I think that you just need to run lampp
+        Mac: open XAMPP Controll.app and click on the appropriate button to start Apache
+    Point you browser to: http://localhost/icloud/icloud.php.
+    Fill in the form and click on Evaluate.
+    Copy the URLs you need and stop Apache again.
+
+
+------------
+Side effects
+------------
+
+Before providing you the download links I want to tell you one interesting side effect I was notified about.
+If you want to use the iCloud CardDAV (address book) service with third-party clients it should work if you just use the principal URL with the appropriate iCloud server: https://p01-contacts.icloud.com/<PRINCIPAL_URL>.
+Please note that you can use the servers p01 to p08 if you want! (I added a select field in the script for that.) 
+
+
+-------------------------
+Download and restrictions
+-------------------------
+
+First of all, I want to list some restrictions:
+
+    You are not allowed to make the script or the download archive publicly available or to republish them.
+    You are not allowed to modify any of the files.
+    You are only allowed to use it for your personal use on your only locally reachable server. (localhost)
+
+Additionally, I want to refer you to the appropriate blog post (http://blog.muehlbachler.org/2011/08/how-to-icloud-calendars-with-third-party-software/) where I will post updates if there are any.
+
+So, here is the download:
+
+    .tar.gz/.tgz: http://icloud.niftyside.com/icloud.tgz
+    .zip: http://icloud.niftyside.com/icloud.zip
+
+
+-----------------
+Copyright notices
+-----------------
+
+This script is copyright (C) 2011 by NiftySide - Daniel Mühlbachler (http://www.niftyside.com).
+iCloud is a service and is provided by Apple, Inc. (http://www.apple.com)
---
+++ icloud.php
@@ -0,0 +1,214 @@
+<html>
+	<head>
+    	<title>iCloud principal and calendar settings</title>
+        <style type="text/css">
+			table {
+				border-collapse:collapse;
+				border:1px solid;
+				border-color:#999;
+			}
+			table td {
+				padding:10px 20px;
+			}
+			
+			#top_bar {
+				overflow:hidden;
+				width:100%;
+				height:270px;
+			}
+			
+			#result {
+				border:1px solid #CCC;
+				overflow:auto;
+				width:99%;
+				top:270px;
+				bottom:26px;
+				position:absolute;
+			}
+			
+			#copy {
+				overflow:hidden;
+				width:99%;
+				bottom:0px;
+				position:absolute;
+				height:25px;
+				margin-left:5px;
+			}
+			#copy * {
+				font-size:12px;
+				color:#333;
+			}
+			#copy div {
+				padding-top:3px;
+			}
+			
+			a, #copy a {
+				color:#960;
+				text-decoration:none;
+			}
+			a:hover, a:focus, #copy a:hover, #copy a:focus {
+				text-decoration:underline;
+				color:#300;
+			}
+		</style>
+    </head>
+    <body onLoad="document.getElementById('appleID').focus();">
+<?php
+	//Define iCloud URLs
+	//define("ICLOUD_URL", "https://p01-caldav.icloud.com");
+	$icloudUrls = array();
+	for($i = 1; $i < 9; $i++)
+		$icloudUrls[] = "https://p0".$i."-caldav.icloud.com";
+	
+	//Functions
+	function doRequest($user, $pw, $url, $xml)
+	{
+		//Init cURL
+		$c=curl_init($url);
+		//Set headers
+		curl_setopt($c, CURLOPT_HTTPHEADER, array("Depth: 1", "Content-Type: text/xml; charset='UTF-8'"));
+		curl_setopt($c, CURLOPT_HEADER, 0);
+		//Set SSL
+		curl_setopt($c, CURLOPT_SSL_VERIFYHOST, 0);
+		curl_setopt($c, CURLOPT_SSL_VERIFYPEER, 0);
+		//Set HTTP Auth
+		curl_setopt($c, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+		curl_setopt($c, CURLOPT_USERPWD, $user.":".$pw);
+		//Set request and XML
+		curl_setopt($c, CURLOPT_CUSTOMREQUEST, "PROPFIND");
+		curl_setopt($c, CURLOPT_POSTFIELDS, $xml);
+		curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+		//Execute
+		$data=curl_exec($c);
+		//Close cURL
+		curl_close($c);
+		
+		return $data;
+	}
+?>
+		<div id="top_bar">
+            <h1 style='color:darkred;'>Provide your iCloud login settings:</h1>
+            <form action="" method="post">
+                <table border='0'>
+                    <tr>
+                        <td><b style='color:blue;'>Apple ID: </b></td>
+                        <td><input type='text' name='appleID' id='appleID' size='50' value='<? echo isset($_POST['appleID']) ? $_POST['appleID'] : "";?>'></td>
+                    </tr>
+                    <tr>
+                        <td><b style='color:blue;'>Password: </b></td>
+                        <td><input type='password' name='pw' id='pw' size='50' value='<? echo isset($_POST['appleID']) ? $_POST['pw'] : "";?>'></td>
+                    </tr>
+                    <tr>
+                    	<td><b style='color:blue;'>iCloud server: </b></td>
+                        <td><select name='server' id='server'>
+<?php
+	$set = false;
+	foreach($icloudUrls as $server) {
+		echo "<option value='".$server."'";
+		if(isset($_POST['server']) AND $_POST['server'] == $server)
+			echo " selcted";
+		else if(!isset($_POST['server']) AND !$set) {
+			echo " selected";
+			$set = true;
+		}
+		echo ">".$server."</option>";
+	}
+?>
+							</select>
+						</td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" align="center"><input type='submit' value='Evaluate'></td>
+                    </tr>
+                </table>
+            </form><br>
+        </div>
+        <div id="result">
+<?php
+	if(isset($_POST['appleID']))
+	{
+		$user=$_POST['appleID'];
+		$pw=$_POST['pw'];
+		
+		//Get Principal URL
+		$principal_request="<A:propfind xmlns:A='DAV:'>
+								<A:prop>
+									<A:current-user-principal/>
+								</A:prop>
+							</A:propfind>";
+		$response=simplexml_load_string(doRequest($user, $pw, $_POST['server'], $principal_request));
+		//Principal URL
+		$principal_url=$response->response[0]->propstat[0]->prop[0]->{'current-user-principal'}->href;
+		$userID=explode("/", $principal_url);
+		$userID=$userID[1];
+		
+		
+		//Get Calendars
+		$calendars_request="<A:propfind xmlns:A='DAV:'>
+								<A:prop>
+									<A:displayname/>
+								</A:prop>
+							</A:propfind>";
+		$url=$_POST['server']."/".$userID."/calendars/";
+		$response=simplexml_load_string(doRequest($user, $pw, $url, $calendars_request));
+		//To array
+		$calendars=array();
+		foreach($response->response as $cal)
+		{
+			$entry["href"]=$cal->href;
+			$entry["name"]=$cal->propstat[0]->prop[0]->displayname;
+			$calendars[]=$entry;
+		}
+		
+		
+		//Output
+		echo "<h1 style='color:darkred;'>Your principal settings</h1>";
+		echo "<table border='1' style='border-collapse:collapse;'>
+				<tr>
+					<td><b style='color:blue;'>iCloud CalDAV server:</b></td>
+					<td style='color:darkgreen;'>".$_POST['server']."</td>
+				</tr>
+				<tr>
+					<td><b style='color:blue;'>User-ID: </b></td>
+					<td style='color:darkgreen;'>".$userID."</td>
+				</tr>
+				<tr>
+					<td><b style='color:blue;'>Principal-URL: </b></td>
+					<td style='color:darkgreen;'>".$principal_url."</td>
+				</tr>
+			</table><br>";
+		echo "<h1 style='color:darkred;'>Your calendars</h1>";
+		echo "<table border='1' style='border-collapse:collapse;'>
+				<tr>
+					<td align='center'><b style='color:blue;'>Calendar</td>
+					<td align='center'><b style='color:blue;'>Calendar href/td>
+					<td align='center'><b style='color:blue;'>URL</td>
+				</tr>";
+		foreach($calendars as $calendar)
+			echo "<tr>
+					<td style='color:darkgreen;'>".$calendar["name"]."</td>
+					<td>".$calendar["href"]."</td>
+					<td>".$_POST['server'].$calendar["href"]."</td>
+				</tr>";
+		echo "</table><br>";
+	}
+	else
+	{
+?>
+		<br>
+        &nbsp;&nbsp;&nbsp;<i>Please provide your login credentials first.</i>
+<?php
+	}
+?>
+		</div>
+		<div id="copy">
+        	<div>Script copyright &copy; 2011 by <a href='http://www.niftyside.com' target="_blank">NiftySide - Daniel M&uuml;hlbachler</a>
+            &nbsp;&nbsp;&nbsp;;&nbsp;&nbsp;&nbsp;
+            <a href="http://www.icloud.com" target="_blank">iCloud</a> is a service provided by <a href="http://www.apple.com" target="_blank">Apple Inc.</a></div>
+        </div>
+	</body>
+</html>
+
+<!-- Copyright (C) 2011 by NiftySide - Daniel Muehlbachler (http://www.niftyside.com) -->
+<!-- You are not allowed to remove the copyright notices anywhere in this document! -->
+<!-- Please read the dedicated README file for further information on the usage and copyright! -->
